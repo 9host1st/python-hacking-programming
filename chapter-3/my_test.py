cb4f39859f4cd290b3675fa33f224a39c46dc29a
9host1st <9host1st@gmail.com>
2022-08-14 03:45:28 +0900

    2022-08-14

---
+++ chapter-3/my_test.py
@@ -0,0 +1,11 @@
+import my_debugger
+
+debugger = my_debugger.debugger()
+
+pid = raw_input("Enter the PID of the process to attach to: ")
+
+debugger.attach(int(pid))
+
+debugger.run()
+
+debugger.detach()
